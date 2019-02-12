1f62cab306a4ee6c6beced41b0ad9336547a7872
Булгаков Сергей <sanyo3102@gmail.com>
2019-02-12 12:56:54 +0300

    game working

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ import os
 from shapes import *
 
 FPS = 60
-TILESIZE = 64
-resolution = WIDTH, HEIGHT = 1024, 896
+TILESIZE = 32
+resolution = WIDTH, HEIGHT = 634, 800
 GAME_WIDTH, GAME_HEIGHT = 320, 640
+columns = 10
+rows = 20
 
 START_MOVE_TIME = 60
 MOVE_TIME_INCREASE = 1.5
+DIFFICULTY = 0.85
 
 
 def load_image(name, colorkey=None):
@@ -25,29 +28,6 @@ def load_image(name, colorkey=None):
             colorkey = image.get_at((0, 0))
         image.set_colorkey(colorkey)
     return image
-
-
-def create_grid(pre_init_pos={}):
-    grid = [[0 for _ in range(10)] for _ in range(20)]
-    for i in range(20):
-        for j in range(10):
-            if (i, j) not in pre_init_pos:
-                grid[i][j] = pygame.sprite.Sprite(static_tiles)
-                grid[i][j].image = load_image('black_tile.png')
-                grid[i][j].rect = grid[i][j].image.get_rect()
-                grid[i][j].rect.x = top_left[0] + j * (TILESIZE // 2)
-                grid[i][j].rect.y = top_left[1] + i * (TILESIZE // 2)
-    return grid
-
-
-def get_random_shape():
-    return random.choice(shapes)
-
-
-def draw_grid(surface):
-    surface.blit(game_background, (0, 0))
-    #  pygame.draw.rect(surface, (0, 191, 255), (top_left[0] - 10, top_left[1] - 10, GAME_WIDTH + 20, GAME_HEIGHT + 20))
-    static_tiles.draw(surface)
 
 
 class FlickeringSprite(pygame.sprite.Sprite):
@@ -80,13 +60,186 @@ class FlickeringSprite(pygame.sprite.Sprite):
             self.time_to_timeout = 0
 
 
-class FlyingLetter:
-    def __init__(self, x, y, shape):
-        self.x = x
-        self.y = y
-        self.shape = shape
-        self.image = tile_images
-        self.rotation = 0
+class Randomizer:
+    def __init__(self, items_num):
+        self.current_item = 0
+        self.items = [i for i in range(items_num)]
+        random.shuffle(self.items)
+
+    def get_number(self):
+        if self.current_item == len(self.items):
+            self.current_item = 0
+            self.shuffle()
+
+        item = self.items[self.current_item]
+        self.current_item += 1
+        return item
+
+    def shuffle(self):
+        random.shuffle(self.items)
+
+    def reset(self):
+        self.current_item = 0
+        self.shuffle()
+
+
+class Grid:
+    def __init__(self, columns, rows, block_dimensions, screen_res):
+        self.score = 0
+        self.game_over = False
+
+        self.block_width = block_dimensions[0]
+        self.block_height = block_dimensions[1]
+
+        self.area_width = columns * self.block_width
+        self.area_height = rows * self.block_height
+
+        # top left corner of game area
+        self.min_coord = ((screen_res[0] // 2 - self.area_width + 29),
+                          (screen_res[1] - self.area_height) / 2 + 50)
+        # bottom right corner of game area
+        self.max_coord = (self.min_coord[0] + self.area_width,
+                          self.min_coord[1] + self.area_height)
+
+        # center coord of game area (letter start coord)
+        self.center_coord = (self.min_coord[0] + (columns // 2) * self.block_width,
+                             self.min_coord[1] + self.block_height)
+
+        self.columns = columns
+        self.rows = rows
+        # rows x columns grid
+        self.grid = [[-1 for i in range(self.columns)] for j in range(self.rows)]
+
+        self.background = load_image('black_background.png')
+        self.background = pygame.transform.scale(self.background, (self.area_width, self.area_height))
+        self.background.set_alpha(200)
+
+    def collided(self, letter_coords):
+        indexes_list = self.convert_coords(letter_coords)
+        for row_index, column_index in indexes_list:
+            if row_index >= self.rows or self.grid[row_index][column_index] >= 0:
+                return True
+        return False
+
+    def is_out_of_bounds(self, letter_coords):
+        for x, y in letter_coords:
+            if x > self.max_coord[0] - self.block_width or x < self.min_coord[0]:
+                return True
+        return False
+
+    def is_game_over(self):
+        return self.game_over
+
+    def convert_coords(self, coords):
+        indexes_list = []
+        for coord in coords:
+            column_index = int((coord[0] - self.min_coord[0]) // self.block_width)
+            row_index = int((coord[1] - self.min_coord[1]) // self.block_height)
+            indexes_list.append((row_index, column_index))
+        return indexes_list
+
+    def convert_indexes(self, indexes):
+        coords_list = []
+        for index in indexes:
+            x = int(index[1] * self.block_width + self.min_coord[0])
+            y = int(index[0] * self.block_height + self.min_coord[1])
+            coords_list.append((x, y))
+        return coords_list
+
+    def update(self, coords, color_index):
+        indexes_list = self.convert_coords(coords)
+        for row_index, column_index in indexes_list:
+            if row_index >= 0 and column_index >= 0:
+                self.grid[row_index][column_index] = color_index
+
+        score = 0
+        # search for full rows
+        for row_index, column_index in indexes_list:
+            # check game over
+            if row_index == 0:  # there is a block at the first row
+                self.game_over = True
+
+            full_row = True
+            for j in range(self.columns):
+                if self.grid[row_index][j] == -1:  # cell is empty
+                    full_row = False
+                    break
+
+            # delete the row if it is full
+            if full_row:
+                del self.grid[row_index]
+                score += 1
+                # insert a new line at the beginning of the grid
+                self.grid.insert(0, [-1 for i in range(self.columns)])
+        if score == 1:
+            self.score += 100
+        elif score == 2:
+            self.score += 300
+        elif score == 3:
+            self.score += 700
+        elif score == 4:
+            self.score += 1500
+        else:
+            self.score += score * 4 * 150
+
+    def get_assist_coords(self, letter_coords):
+        indexes_list = self.convert_coords(letter_coords)
+        bottom = False
+        collided = False
+
+        # letter reached bottom
+        for row_index, column_index in indexes_list:
+            if row_index >= self.rows - 1:
+                return letter_coords
+
+        while not bottom and not collided:
+            # for every block
+            for i in range(len(indexes_list)):
+                row_index, column_index = indexes_list[i]
+                indexes_list[i] = (row_index + 1, column_index)
+                # check next row
+                if self.grid[row_index + 1][column_index] >= 0:
+                    collided = True
+                elif row_index + 1 >= self.rows - 1:
+                    bottom = True
+
+        if collided:
+            i = 0
+            for row_index, column_index in indexes_list:
+                indexes_list[i] = (row_index - 1, column_index)
+                i += 1
+        return self.convert_indexes(indexes_list)
+
+    def show(self, screen, color_blocks):
+        screen.blit(self.background, self.min_coord)
+        for i in range(self.rows):
+            for j in range(self.columns):
+                if self.grid[i][j] >= 0:  # if cell isn't empty (empty -> -1)
+                    # cell value is color index
+                    color_index = self.grid[i][j]
+                    coord_x = self.min_coord[0] + j * self.block_width
+                    coord_y = self.min_coord[1] + i * self.block_height
+                    screen.blit(color_blocks[color_index], (coord_x, coord_y))
+
+    def display_message(self, screen, font, color, message):
+        text_surface = font.render(str(message), True, color).convert_alpha()
+        text_x = self.min_coord[0] + (self.area_width - text_surface.get_width()) / 2
+        text_y = (self.min_coord[1] + self.area_height) / 2
+        screen.blit(text_surface, (text_x, text_y))
+        pygame.display.flip()
+
+    def get_score(self):
+        return self.score
+
+    def get_center_coord(self):
+        return self.center_coord
+
+
+def write(font, message, color):
+    text = font.render(str(message), True, color)
+    text = text.convert_alpha()
+
+    return text
 
 
 def main_menu(screen):
@@ -111,7 +264,7 @@ def main_menu(screen):
     active_button = 'start'
     while True:
         screen.blit(background, (0, 0))
-        screen.blit(logo, (170, 95))
+        screen.blit(logo, (5, 95))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 exit()
@@ -154,33 +307,166 @@ def main_menu(screen):
         clock.tick(FPS)
 
 
-pygame.mixer.pre_init(44100, -16, 1, 512)
-pygame.init()
-screen = pygame.display.set_mode(resolution)
-pygame.display.set_caption('Tetris')
-clock = pygame.time.Clock()
+if __name__ == '__main__':
+    pygame.mixer.pre_init(44100, -16, 1, 512)
+    pygame.init()
+    screen = pygame.display.set_mode(resolution)
+    pygame.display.set_caption('Tetris')
+    clock = pygame.time.Clock()
+    pygame.key.set_repeat(100, 70)  # TODO: config key repeat delay
 
-main_menu(screen)
-background_image = load_image('background.png')
-game_background = load_image('game_background.png')
+    font = pygame.font.Font(os.path.join('font', 'PressStart2P.ttf'), 34)
+    background = load_image('background.png')
+    game_background = load_image('game_background.png')
 
-tile_images = [load_image('tile1.png'), load_image('tile3.png'), load_image('tile2.png'),
-                   load_image('tile1.png'), load_image('tile3.png'), load_image('tile2.png')]
-# top_left = ((WIDTH - GAME_WIDTH) // 2, HEIGHT - GAME_HEIGHT - 10)
-top_left = (380, 156)
+    randomizer = Randomizer(7)
+    get_number = randomizer.get_number
+    reset = randomizer.reset
 
-static_tiles = pygame.sprite.Group()
-grid = create_grid()
+    blocks = [load_image('tile1.png'), load_image('tile2.png'), load_image('tile3.png'), load_image('tile1.png'),
+              load_image('tile2.png'), load_image('tile3.png'), load_image('tile1.png')]
+    next_letter_coord = (442, 430)
+    next_coord = (next_letter_coord[0] - 10, next_letter_coord[1] - 3 * TILESIZE)
+    top_left = (380, 156)
+    score_coord = (413, 200)
+    time_coord = (18, 33)
+    level_coord = (446, 615)
+    highscore_coord = (416, 109)
 
-running = True
-while running:
-    screen.fill((0, 0, 0))
-    draw_grid(screen)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            exit()
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RETURN:
-                pass
-    clock.tick(FPS)
-    pygame.display.flip()
+    menu_running = True
+    while menu_running:
+        randomizer.reset()
+        display_menu = True
+        main_menu(screen)
+
+        if os.path.isfile('highscores.txt'):
+            with open('highscores.txt', 'r') as file:
+                highscore = file.readlines()
+                highscore = [int(i.replace('\n', '')) for i in highscore]
+                highscore = str(max(highscore))
+        else:
+            highscore = '0'
+
+        highscore_surface = write(font, highscore, (255, 255, 255))
+        next_surface = write(font, "", (0, 0, 0))
+        grid = Grid(columns, rows, (TILESIZE, TILESIZE), resolution)
+        letter_move_time = 1
+        current_letter = FlyingLetter((TILESIZE, TILESIZE), grid.get_center_coord(), letter_move_time, randomizer)
+        next_letter = FlyingLetter((TILESIZE, TILESIZE), next_letter_coord, letter_move_time, randomizer)
+
+        clock = pygame.time.Clock()
+        total_time = 0.0
+        difficulty_level = 1
+        dt = 1.0 / FPS
+        accumulator = 0.0
+        game_running = True
+        game_paused = drop_tetromino = False
+        while game_running:
+            screen.fill((0, 0, 0))
+            screen.blit(game_background, (0, 0))
+            counter = 1
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        game_running = False
+                    elif event.key == pygame.K_RIGHT:
+                        current_letter.move_right()
+                        if grid.is_out_of_bounds(current_letter.get_coords()) or \
+                                grid.collided(current_letter.get_coords()):
+                            current_letter.move_left()
+                    elif event.key == pygame.K_LEFT:
+                        current_letter.move_left()
+                        if grid.is_out_of_bounds(current_letter.get_coords()) or \
+                                grid.collided(current_letter.get_coords()):
+                            current_letter.move_right()
+                    elif event.key == pygame.K_DOWN:
+                        current_letter.rotate_ccw()
+                        # if letter collides or it is out of bounds undo rotate counterclockwise
+                        if grid.is_out_of_bounds(current_letter.get_coords()) or \
+                                grid.collided(current_letter.get_coords()):
+                            current_letter.rotate_cw()
+                    elif event.key == pygame.K_UP:
+                        current_letter.rotate_cw()
+                        # if letter collides or it is out of bounds undo rotate clockwise
+                        if grid.is_out_of_bounds(current_letter.get_coords()) or \
+                                grid.collided(current_letter.get_coords()):
+                            current_letter.rotate_ccw()
+                    elif event.key == pygame.K_SPACE:
+                        current_letter.speed_up()
+                    elif event.key == pygame.K_p:
+                        pause = True
+                        grid.display_message(screen, font, (0, 0, 0), 'PAUSE')
+                        while pause:
+                            event = pygame.event.wait()
+                            if event.type == pygame.KEYDOWN and event.key == pygame.K_p:
+                                pause = False
+                            elif event.type == pygame.QUIT:
+                                exit()
+                        game_paused = True
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_SPACE:
+                        current_letter.reset_speed()
+
+            if game_paused:
+                frame_time = dt
+                game_paused = False
+                clock.tick(FPS)
+            else:
+                frame_time = clock.tick(FPS) / 1000.0  # convert to seconds
+            accumulator += frame_time
+            while accumulator >= dt and game_running:
+                while True:
+                    current_letter.move_down(dt)
+                    collided = grid.collided(current_letter.get_coords())
+                    if not drop_tetromino or collided:
+                        drop_tetromino = False
+                        break
+
+                if collided:
+                    current_letter.move_up()
+                    grid.update(current_letter.get_coords(), current_letter.get_color_index())
+                    # increase difficulty level every time 4000 points are claimed
+                    if grid.get_score() / 4000 >= difficulty_level:
+                        difficulty_level += 1
+                        letter_move_time *= DIFFICULTY
+
+                    current_letter = next_letter
+                    current_letter.set_coords(grid.get_center_coord())
+                    current_letter.set_speed(letter_move_time)
+                    next_letter = FlyingLetter((TILESIZE, TILESIZE), next_letter_coord, letter_move_time, randomizer)
+                game_running = not grid.is_game_over()
+                accumulator -= dt
+            total_time += frame_time
+            time_string = "TIME " + '{0:02d}'.format(int(total_time // 60))\
+                          + ":" + '{0:02d}'.format(int(total_time % 60))
+            time_surface = write(font, time_string, (255, 255, 255))
+
+            score_string = str(grid.get_score())
+            score_surface = write(font, score_string, (255, 255, 255))
+            level_string = str(difficulty_level)
+            level_surface = write(font, level_string, (255, 255, 255))
+
+            # Draw
+            # screen.blit(background, (0, 0))
+            screen.blit(score_surface, score_coord)
+            screen.blit(level_surface, level_coord)
+            screen.blit(time_surface, time_coord)
+            screen.blit(next_surface, next_coord)
+            screen.blit(highscore_surface, highscore_coord)
+            grid.show(screen, blocks)
+            if game_running:
+                current_letter.show(screen, blocks)
+            next_letter.show(screen, blocks)
+            pygame.display.flip()
+            clock.tick(FPS)
+            pygame.display.flip()
+
+        if os.path.isfile('highscores.txt'):
+            with open('highscores.txt', 'a') as file:
+                file.write(str(grid.get_score()) + '\n')
+        else:
+            with open('highscores.txt', 'w') as file:
+                file.write(str(grid.get_score()) + '\n')
+
--- shapes.py
+++ shapes.py
@@ -1,103 +1,261 @@
-S = [['.....',
-      '......',
-      '..00..',
-      '.00...',
-      '.....'],
-     ['.....',
-      '..0..',
-      '..00.',
-      '...0.',
-      '.....']]
+class FlyingLetter:
+    i_letter_angle_0 = ["....",
+                           "####",
+                           "....",
+                           "...."]
 
-Z = [['.....',
-      '.....',
-      '.00..',
-      '..00.',
-      '.....'],
-     ['.....',
-      '..0..',
-      '.00..',
-      '.0...',
-      '.....']]
+    # rotate 90 degrees cw (clockwise)
+    i_letter_angle_90 = [".#..",
+                            ".#..",
+                            ".#..",
+                            ".#.."]
 
-I = [['..0..',
-      '..0..',
-      '..0..',
-      '..0..',
-      '.....'],
-     ['.....',
-      '0000.',
-      '.....',
-      '.....',
-      '.....']]
+    i_letter = (i_letter_angle_0, i_letter_angle_90,
+                   i_letter_angle_0, i_letter_angle_90)
 
-O = [['.....',
-      '.....',
-      '.00..',
-      '.00..',
-      '.....']]
+    # O letter
+    o_letter_angle_0 = ["##..",
+                           "##..",
+                           "....",
+                           "...."]
 
-J = [['.....',
-      '.0...',
-      '.000.',
-      '.....',
-      '.....'],
-     ['.....',
-      '..00.',
-      '..0..',
-      '..0..',
-      '.....'],
-     ['.....',
-      '.....',
-      '.000.',
-      '...0.',
-      '.....'],
-     ['.....',
-      '..0..',
-      '..0..',
-      '.00..',
-      '.....']]
+    o_letter = (o_letter_angle_0, o_letter_angle_0,
+                   o_letter_angle_0, o_letter_angle_0)
 
-L = [['.....',
-      '...0.',
-      '.000.',
-      '.....',
-      '.....'],
-     ['.....',
-      '..0..',
-      '..0..',
-      '..00.',
-      '.....'],
-     ['.....',
-      '.....',
-      '.000.',
-      '.0...',
-      '.....'],
-     ['.....',
-      '.00..',
-      '..0..',
-      '..0..',
-      '.....']]
+    # T letter
+    t_letter_angle_0 = [".#..",
+                           "###.",
+                           "....",
+                           "...."]
 
-T = [['.....',
-      '..0..',
-      '.000.',
-      '.....',
-      '.....'],
-     ['.....',
-      '..0..',
-      '..00.',
-      '..0..',
-      '.....'],
-     ['.....',
-      '.....',
-      '.000.',
-      '..0..',
-      '.....'],
-     ['.....',
-      '..0..',
-      '.00..',
-      '..0..',
-      '.....']]
+    # rotate 90 degrees cw
+    t_letter_angle_90 = [".#..",
+                            ".##.",
+                            ".#..",
+                            "...."]
 
-shapes = [S, Z, I, O, J, L, T]
+    # rotate 180 degrees cw
+    t_letter_angle_180 = ["....",
+                             "###.",
+                             ".#..",
+                             "...."]
+
+    # rotate 270 degrees cw
+    t_letter_angle_270 = [".#..",
+                             "##..",
+                             ".#..",
+                             "...."]
+
+    t_letter = (t_letter_angle_0, t_letter_angle_90,
+                   t_letter_angle_180, t_letter_angle_270)
+
+    # J letter
+    j_letter_angle_0 = ["#...",
+                           "###.",
+                           "....",
+                           "...."]
+
+    # rotate 90 degrees cw
+    j_letter_angle_90 = [".##.",
+                            ".#..",
+                            ".#..",
+                            "...."]
+
+    # rotate 180 degrees cw
+    j_letter_angle_180 = ["....",
+                             "###.",
+                             "..#.",
+                             "...."]
+
+    # rotate 270 degrees cw
+    j_letter_angle_270 = [".#..",
+                             ".#..",
+                             "##..",
+                             "...."]
+
+    j_letter = (j_letter_angle_0, j_letter_angle_90,
+                   j_letter_angle_180, j_letter_angle_270)
+
+    # L letter cw
+    l_letter_angle_0 = ["..#.",
+                           "###.",
+                           "....",
+                           "...."]
+
+    # rotate 90 degrees cw
+    l_letter_angle_90 = [".#..",
+                            ".#..",
+                            ".##.",
+                            "...."]
+
+    # rotate 180 degrees cw
+    l_letter_angle_180 = ["....",
+                             "###.",
+                             "#...",
+                             "...."]
+
+    # rotate 270 degrees cw
+    l_letter_angle_270 = ["##..",
+                             ".#..",
+                             ".#..",
+                             "...."]
+
+    l_letter = (l_letter_angle_0, l_letter_angle_90,
+                   l_letter_angle_180, l_letter_angle_270)
+
+    # S letter
+    s_letter_angle_0 = [".##.",
+                           "##..",
+                           "....",
+                           "...."]
+
+    # rotate 90 degrees cw
+    s_letter_angle_90 = [".#..",
+                            ".##.",
+                            "..#.",
+                            "...."]
+
+    s_letter = (s_letter_angle_0, s_letter_angle_90,
+                   s_letter_angle_0, s_letter_angle_90)
+
+    # Z letter
+    z_letter_angle_0 = ["##..",
+                           ".##.",
+                           "....",
+                           "...."]
+
+    # rotate 90 degrees cw
+    z_letter_angle_90 = ["..#.",
+                            ".##.",
+                            ".#..",
+                            "...."]
+
+    z_letter = (z_letter_angle_0, z_letter_angle_90,
+                   z_letter_angle_0, z_letter_angle_90)
+
+    letters = (i_letter, o_letter, t_letter, j_letter,
+                   l_letter, s_letter, z_letter)
+
+    fast_move_time = 0.03  # seconds
+
+    def __init__(self, block_dimensions, coord, move_time, randomizer):
+        self.random_index = randomizer.get_number()
+        self.random_letter = FlyingLetter.letters[self.random_index]
+
+        self.current_angle = 0
+        self.current_frame = self.random_letter[self.current_angle]
+
+        self.block_width = block_dimensions[0]
+        self.block_height = block_dimensions[1]
+
+        # coordinates of the center block (required for the rotation)
+        self.center_coord = list(coord)
+
+        # coordinates for each block of the letter
+        # list of coordinates [x, y]
+        self.blocks_coords = self.build()
+
+        # time in seconds
+        self.normal_move_time = move_time
+        self.move_time = self.normal_move_time
+        self.elapsed_time = 0.0
+
+    def build(self):
+        """
+        Calculates the coordinates of each block that is part of the letter.
+        center block index is (1, 1)
+        :return: list of coordinates [x, y] or empty list if a block is out of bounds
+        """
+        x, y = [self.center_coord[0] - self.block_width, self.center_coord[1]
+                - self.block_height]
+        letter_coords = []
+
+        for i in range(len(self.current_frame)):
+            for char in self.current_frame[i]:
+                if char == '#':
+                    letter_coords.append([x, y])
+                x += self.block_width
+            x = self.center_coord[0] - self.block_width
+            y += self.block_height
+        return letter_coords
+
+    def set_coords(self, center_coord):
+        self.center_coord = list(center_coord)
+        self.blocks_coords = self.build()
+
+    def get_coords(self):
+        return self.blocks_coords
+
+    def get_color_index(self):
+        return self.random_index
+
+    def speed_up(self):
+        self.move_time = FlyingLetter.fast_move_time
+
+    def reset_speed(self):
+        self.move_time = self.normal_move_time
+
+    def set_speed(self, move_time):
+        self.normal_move_time = move_time
+        self.move_time = self.normal_move_time
+
+    def show(self, screen, color_blocks):
+        """
+        Display all blocks.
+        :param screen: screen surface
+        :param color_blocks: block surfaces tuple
+        """
+        for coord in self.blocks_coords:
+            screen.blit(color_blocks[self.random_index], coord)
+
+    def move_up(self):
+        for coord in self.blocks_coords:
+            coord[1] -= self.block_height
+        self.center_coord[1] -= self.block_height
+
+    def move_down(self, time):
+        """
+        Move down all blocks.
+        """
+        self.elapsed_time += time
+        if self.elapsed_time >= self.move_time:
+            self.elapsed_time = 0
+            for coord in self.blocks_coords:
+                coord[1] += self.block_height
+            self.center_coord[1] += self.block_height
+
+    def move_left(self):
+        """
+        Move left all blocks.
+        """
+        self.center_coord[0] -= self.block_width
+        for coord in self.blocks_coords:
+            coord[0] -= self.block_width
+
+    def move_right(self):
+        """
+        Move right all blocks.
+        """
+        self.center_coord[0] += self.block_width
+        for coord in self.blocks_coords:
+            coord[0] += self.block_width
+
+    def rotate_ccw(self):
+        """
+        Rotate the letter 90 degrees counterclockwise.
+        """
+        if self.current_angle == 0:
+            self.current_angle = 3
+        else:
+            self.current_angle -= 1
+
+        self.current_frame = self.random_letter[self.current_angle]
+        self.blocks_coords = self.build()
+
+    def rotate_cw(self):
+        """
+        Rotate the letter 90 degrees clockwise.
+        """
+        self.current_angle = (self.current_angle + 1) % 4
+        self.current_frame = self.random_letter[self.current_angle]
+        self.blocks_coords = self.build()
